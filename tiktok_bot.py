e62d39a932d58447906e5a1d54c8d53181612268
abobakrweka447-source <abobakrweka447@gmail.com>
2026-06-25 03:19:35 +0300

    Clean up tiktok_bot.py by removing imports
    Removed unused imports and keep_alive function call.

--- tiktok_bot.py
+++ tiktok_bot.py
@@ -1,10 +1,3 @@
-import sys, os
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from keep_alive import keep_alive
-
-keep_alive()
-
 import re
 import math
 import time
